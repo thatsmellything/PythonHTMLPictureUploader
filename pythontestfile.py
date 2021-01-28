a1e9f848db689808951f4f782afb8d9c70daad2d
Jensen Judkins <thesmelliestman@gmail.com>
2021-01-27 17:43:55 -0700

    Got it to work!

--- pythontestfile.py
+++ pythontestfile.py
@@ -8,14 +8,25 @@ print("Hello world")
 #f.close()
 
 #for each f in pics
-
-
 jpgfiles = []
 for file in glob.glob("pics/*.txt"):
-    jpgfiles.append(file)
+    jpgfiles.append("Add html img tag here"+file)
 print(jpgfiles)
 
+#Writes up the HTML File and stores it into a variable
+with open('testforpicturesP1.html', 'r') as file:
+    data1 = file.read().replace('\n', '')
+									
+with open('testforpicturesP2.html', 'r') as file:
+    data2 = file.read().replace('\n', '')
+
+
+
 #Writes the list to the file
-with open('listofjpg.txt', 'w') as f:
+with open('testforpictures.html', 'w') as f:
+    f.write(data1)
     for item in jpgfiles:
         f.write("%s\n" % item)
+    f.write(data2)
+
+f.close()
